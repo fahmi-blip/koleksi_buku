fc544e75b3259c9fe8d40ed5518588fed1eafe0d
fahmi-blip <fahmisyhb9@gmail.com>
2026-02-11 21:12:35 +0700

    first

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+@include('layouts.header')
+
+      <body>
+        <div class="container-scroller">
+          @include('layouts.navbar')
+          <div class="container-fluid page-body-wrapper">
+            @include('layouts.sidebar')
+            <div class="main-panel">
+              <div class="content-wrapper">
+              @yield('content')
+              </div>
+            @include('layouts.footer')
+        </div>
+        </div>
+        </div>
+        @include('layouts.scripts')
+      </body>
